eb988bf0572529c10c15ff9f65c314dcb1649d98
Merlin Unterfinger <info@munterfinger.ch>
2022-07-19 19:37:09 +0200

    Fix segmentation fault due to wrong direction in connection routing and adjust allocation sizes.

--- tests/io_test.cpp
+++ tests/io_test.cpp
@@ -5,10 +5,14 @@
 
 extern "C" {
 #include <osurs/io.h>
+#include <osurs/reserve.h>
 }
 
-// Read a network
-TEST(IOTest, Matsim) {
+/**
+ * @brief Read MATSim transit schedule.
+ *
+ */
+TEST(IOTest, ReadMatsim) {
     // File paths
     int success;
     const char* schedule_file = "input/matsim/transitSchedule.xml";
@@ -25,14 +29,26 @@ TEST(IOTest, Matsim) {
     EXPECT_EQ(network->node_counter, 17);
     EXPECT_EQ(network->route_counter, 2);
 
+    // Check connection: Success
+    Node* n1 = get_node(network, "stop1");
+    Node* n2 = get_node(network, "stop4");
+    Connection* con1 = new_connection(n1, n2, 0);
+    ASSERT_TRUE(con1 != NULL);
+
+    // Check connection: Wrong direction
+    Connection* con2 = new_connection(n2, n1, 0);
+    ASSERT_TRUE(con2 == NULL);
+
     // Cleanup
+    delete_connection(con1);
+    delete_connection(con2);
     delete_network(network);
 }
 
 TEST(IOTest, Read) {
     EXPECT_EQ(print_cwd(), 1);
-    EXPECT_EQ(print_file("input/matsim/transitSchedule.xml"), 1);
-    EXPECT_EQ(print_file("input/matsim/transitVehicles.xml"), 1);
+    // EXPECT_EQ(print_file("input/matsim/transitSchedule.xml"), 1);
+    // EXPECT_EQ(print_file("input/matsim/transitVehicles.xml"), 1);
 }
 
 // Wrtie a network
